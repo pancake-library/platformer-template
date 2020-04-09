e0acbe727c0d3e12c7eab2227f9796d72ce4f3e7
Mighty Pancake <33265188+MightyPancake@users.noreply.github.com>
2020-04-09 14:05:44 +0200

    LÖVE configuration file

--- conf.lua
+++ conf.lua
@@ -4,29 +4,3 @@ function love.conf (t)
 	t.window.fullscreen = true -- Enable fullscreen (boolean)
 	t.window.fullscreentype = "desktop"
 end
---[[
-Error
-
-pancake.lua:934: stack overflow
-
-
-Traceback
-
-pancake.lua:934: in function 'isObjectColliding'
-pancake.lua:961: in function 'getSurfaceContact'
-pancake.lua:613: in function 'getStats'
-pancake.lua:525: in function 'getPressure'
-pancake.lua:529: in function 'getPressure'
-pancake.lua:529: in function 'getPressure'
-pancake.lua:529: in function 'getPressure'
-pancake.lua:529: in function 'getPressure'
-pancake.lua:529: in function 'getPressure'
-...
-pancake.lua:755: in function 'getCollisionForces'
-pancake.lua:716: in function 'collide'
-pancake.lua:827: in function 'collideMultiple'
-pancake.lua:911: in function 'move'
-pancake.lua:501: in function 'updateObjects'
-pancake.lua:406: in function 'update'
-main.lua:74: in function 'update'
-[C]: in function 'xpcall']]--
